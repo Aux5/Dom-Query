b485e8d3cbaa7959ba45df8bac029e0365522bbc
Brett Shearin <brettcshearin@hotmail.com>
2018-06-04 15:07:30 -0500

    second commit

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ let sectionHE = document.querySelector(".article__header")
 sectionHE.textContent = "Welcome to Brett's blog"
 
 const allHeaders= document.querySelectorAll(".article__header")
-for (i=0; allHeaders.length; i++){
-allHeaders.classList.add(" Important")
+for (i=0; i < allHeaders.length; i++){
+allHeaders[i].classList.add("Important")
 }
 console.log(allHeaders)
